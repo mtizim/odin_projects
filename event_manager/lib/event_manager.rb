f858d6ec5c673dd88a9fc122585f749587682bf4
mtizim <mtizim1@gmail.com>
2018-01-25 23:25:11 +0100

    event manager

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,21 +1,50 @@
-puts 'Event manager initialized'
+require 'csv'
+require 'google/apis/civicinfo_v2'
+require 'erb'
 
-event_atendees_lines = File.readlines('event_atendees.csv')
-event_atendees_lines = event_atendees_lines.collect do |line|
-  line = line.split(',')
-  {
-    id: line[0],
-    RegDate: line[1],
-    first_Name: line[2],
-    last_Name: line[3],
-    Email_Address: line[4],
-    HomePhone: line[5],
-    Street: line[6],
-    City: line[7],
-    State: line[8],
-    Zipcode: line[9]
-  }
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5,"0")[0..4]
 end
-event_atendees_lines.shift
 
-puts event_atendees_lines[0][:Street]
+def legislators_by_zipcode(zip)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+  begin
+    civic_info.representative_info_by_address(
+      address: zip, 
+      levels: 'country', 
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    ).officials
+  rescue
+    "You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials"
+  end
+end
+
+def save_thank_you_letters(id,form_letter)
+  Dir.mkdir("output") unless Dir.exists?("output")
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename,'w') do |file|
+    file.puts form_letter
+  end
+end
+
+puts "EventManager initialized."
+
+contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
+
+template_letter = File.read "form_letter.erb"
+erb_template = ERB.new template_letter
+
+contents.each do |row|
+  id = row[0]
+  name = row[:first_name]
+  zipcode = clean_zipcode(row[:zipcode])
+  legislators = legislators_by_zipcode(zipcode)
+
+  form_letter = erb_template.result(binding)
+
+  save_thank_you_letters(id,form_letter)
+end
